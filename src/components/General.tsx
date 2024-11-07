9f03ef7327bbd34ad5f528509df3c3cbcbfa5fdf
talitha <talitha.vieira@aluno.ifsp.edu.br>
2024-11-07 14:02:08 -0300

    feat: add component for managing user authentication data

---
+++ src/components/General.tsx
@@ -0,0 +1,41 @@
+import React, { useImperativeHandle, forwardRef, useState } from 'react';
+import { ReactTabulator, ColumnDefinition } from 'react-tabulator';
+import 'react-tabulator/lib/styles.css';
+import 'react-tabulator/css/tabulator.min.css';
+
+interface GeneralData {
+  auth_user: string;
+  auth_password: string;
+  api_port: number;
+}
+
+const General = forwardRef((props, ref) => {
+  const [tableData, setTableData] = useState<GeneralData[]>([]);
+
+  useImperativeHandle(ref, () => ({
+    getData: () => tableData,
+    setData: (data: GeneralData[]) => {
+      setTableData(data);
+    },
+  }));
+
+  const columns: ColumnDefinition[] = [
+    { title: 'User', field: 'auth_user', editor: 'input' },
+    { title: 'Password', field: 'auth_password', editor: 'input' },
+    { title: 'API Port', field: 'api_port', editor: 'number' },
+  ];
+
+  return (
+    <ReactTabulator
+      data={tableData}
+      columns={columns}
+      layout="fitColumns"
+      options={{ resizableColumnFit: true }}
+      events={{
+        dataChanged: (newData: GeneralData[]) => setTableData(newData),
+      }}
+    />
+  );
+});
+
+export default General;
