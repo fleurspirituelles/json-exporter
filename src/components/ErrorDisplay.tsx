4ce089427820e6078ce59ed4cdb3e159a0b2d891
talitha <talitha.vieira@aluno.ifsp.edu.br>
2024-11-07 16:28:28 -0300

    feat: add component for rendering error messages

---
+++ src/components/ErrorDisplay.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface ErrorDisplayProps {
+  message: string | null;
+}
+
+const ErrorDisplay: React.FC<ErrorDisplayProps> = ({ message }) => {
+  if (!message) return null;
+  
+  return <div style={{ color: 'red', marginTop: '10px' }}>{message}</div>;
+};
+
+export default ErrorDisplay;
