27291cb95703a0fc7901923dd52871ba8f1bd83f
talitha <talitha.vieira@aluno.ifsp.edu.br>
2024-11-07 14:04:25 -0300

    refactor: update import paths for table components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React, { useRef } from 'react';
 import './App.css';
-import DataSource from './components/DataSource';
-import General from './components/General';
-import Points from './components/Points';
+import DataSource from './components/Tables/DataSource';
+import General from './components/Tables/General';
+import Points from './components/Tables/Points';
 import FileHandler from './components/FileHandler';
 
 const App: React.FC = () => {
