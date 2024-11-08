911fc0f32aad014b9d71ccb2a308a6f8e363b540
talitha <talitha.vieira@aluno.ifsp.edu.br>
2024-11-08 13:04:19 -0300

    refactor: clean up styles and update variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import './styles/App.css';
 import LoginPage from './pages/LoginPage';
 import DataPage from './pages/DataPage';
 import './styles/variables.css';
